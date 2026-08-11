652b5e00e11d4e3a5715d64bd3d5cd69f301f6ea
iyad iman <163928736+pakyad@users.noreply.github.com>
2026-08-11 14:45:50 +0800

    fix: render one valid approved artwork image

--- lib/art/painted.ts
+++ lib/art/painted.ts
@@ -1,9 +1,4 @@
-import { mockupPart1 } from "./mockup-part-1";
-import { mockupPart2 } from "./mockup-part-2";
-import { mockupPart3 } from "./mockup-part-3";
-import { mockupPart4 } from "./mockup-part-4";
-import { mockupPart5 } from "./mockup-part-5";
-import { mockupPart6 } from "./mockup-part-6";
-import { mockupPart7 } from "./mockup-part-7";
+import { approvedPart1 } from "./approved-master-part-1";
 
-export const PAINTED_MOCKUP = `data:image/jpeg;base64,${mockupPart1}${mockupPart2}${mockupPart3}${mockupPart4}${mockupPart5}${mockupPart6}${mockupPart7}`;
+// Use one valid raster image. Do not concatenate separately encoded JPEG payloads.
+export const PAINTED_MOCKUP = `data:image/jpeg;base64,${approvedPart1}`;
